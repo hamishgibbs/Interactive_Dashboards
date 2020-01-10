3d766dc458389648dd925c1ad916fce88ccc03f0
hamishgibbs <50525951+hamishgibbs@users.noreply.github.com>
2020-01-10 12:25:11 +0000

    tied map into chart

--- Example_Map_Restart/index.js
+++ Example_Map_Restart/index.js
@@ -17,6 +17,84 @@ var pointMarkerOptions = {
     fillOpacity: 1
 };
 
+//var markersLayer = L.LayerGroup();
+//main_map.addLayer(markersLayer);
+
+//get json from file
+var points = $.getJSON("Z_Points.geojson")
+
+//when data is loaded
+$.when(points).done(function(){
+	
+	//defin variable containing json point data
+	var points_json = points.responseJSON
+	
+	//load json point data into the map
+	L.geoJson(points_json, {
+		pointToLayer: function(feature, latlng) {
+			return L.circleMarker(latlng, pointMarkerOptions);
+		}
+	}).addTo(main_map);
+
+	//console.log(points_json)
+	var delayInMilliseconds = 100;
+
+	main_map.on('zoom move', function() {
+			//this is changing every little move of the map 
+			console.log('zoomed moved') 			
+			var map_bounds = main_map.getBounds();
+			
+			//this timeout doesnt seem to be doing anything
+			setTimeout(function() {
+					//console.log(map_bounds._southWest);
+					//console.log(map_bounds._northEast);
+					var map_bounds_north = map_bounds._northEast.lat
+					var map_bounds_east = map_bounds._northEast.lng
+					var map_bounds_south = map_bounds._southWest.lat
+					var map_bounds_west = map_bounds._southWest.lng
+					//console.log(map_bounds_north, map_bounds_east, map_bounds_south, map_bounds_west)
+					//TRY FILTERING THE DATA TO BE WITHIN MAP BOUND
+
+					var plot_data = [];
+
+					for(var i in points_json.features) {
+						//console.log(points_json.features[i].geometry.coordinates)
+						//if latitude is less than map_bounds_north AND more
+						// than map_bounds_south AND longitude is less than
+						// map_bounds_east AND more than map_bounds_west (ie- it is within view):
+						//check this to make sure proper points are included
+						if ((points_json.features[i].geometry.coordinates[1] < map_bounds_north && points_json.features[i].geometry.coordinates[1] > map_bounds_south && points_json.features[i].geometry.coordinates[0] < map_bounds_east && points_json.features[i].geometry.coordinates[0] > map_bounds_west)) {
+							//console.log(points_json.features[i]);
+							plot_data.push({x: points_json.features[i].properties.CASES, y: points_json.features[i].properties.DEATHS})
+							}
+						}
+					console.log(plot_data)
+
+					var ctx = chartCanvas.getContext('2d');
+					var config = {
+					   type: 'scatter',
+					   data: {
+					      //labels: labels,
+					      datasets: [{
+					         label: 'Disease Mortality',
+					         data: plot_data,
+					         backgroundColor: 'rgba(0, 119, 204, 0.3)'
+					      }]
+					   }
+					};
+					var chart = new Chart(ctx, config);
+
+					}, delayInMilliseconds);
+
+
+		});
+
+})
+
+//add in a 1000 ms delay to prevent updating the chart too much
+
+
+/*
 //try adding rodent point data
 //this file was imported into QGIS - retry method in R GEOJSON MUST BE IN THE CORRECT FORMAT FOR IMPORT
 var points = $.getJSON("Z_Points.geojson",function(data){
@@ -42,7 +120,7 @@ $.when(points).done(function(){
 		return {x: e.properties.CASES, y: e.properties.DEATHS}
 	});;
 
-	console.log(data)
+	//console.log(data)
 
 	var ctx = chartCanvas.getContext('2d');
 	var config = {
@@ -57,8 +135,13 @@ $.when(points).done(function(){
 	   }
 	};
 
-//update the chartCanvas variable in the .html document with the data
-var chart = new Chart(ctx, config);
+	//update the chartCanvas variable in the .html document with the data
+	var chart = new Chart(ctx, config);
+
+	//console.log(contained)
+
+
+
 });
 
 
@@ -70,3 +153,4 @@ var chart = new Chart(ctx, config);
 //then how to change the plot for every visible (or selected) feature
 
 //then how to change map from plot
+*/
