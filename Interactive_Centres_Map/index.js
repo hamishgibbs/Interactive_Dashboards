cd1200afa9f602fc03e7ed430949f09c83a03727
hamishgibbs <hamishgibbs@MacBook-Air.local>
2020-01-14 18:32:41 +0000

    Centre Mapping

--- Interactive_Centres_Map/index.js
+++ Interactive_Centres_Map/index.js
@@ -31,6 +31,17 @@ $.when(points).done(function(){
 		//style point data using pointMarkerOptions
 		pointToLayer: function(feature, latlng) {
 			var marker = L.circleMarker(latlng, pointMarkerOptions);
+			
+			marker.on('mouseover', function(e){
+				this.openPopup();
+			});
+
+			marker.on('mouseout', function(e){
+  				marker.closePopup();
+			});
+
+			marker.on('click', onMarkerClick)
+
 			all_markers.push(marker)
 			return marker;
 		}
@@ -38,56 +49,96 @@ $.when(points).done(function(){
 
 	// make this happen on hover
 	for (var i in mapped_points._layers){
-		mapped_points._layers[i].bindPopup(mapped_points._layers[i].feature.properties.SHORT_N)
+		mapped_points._layers[i].bindPopup(mapped_points._layers[i].feature.properties.SHORT_N);
 	}
 
+	/*
+	for (marker in all_markers){
+		marker.on('mouseover', function(e){
+				this.openPopup();
+		});
+	}
+	*/
 	//click and show/hide info panel
 
 });
 
-//wrap in reactive context on marker click
-//get these values from the geojson file
-var project_title = "Big Project"
-var project_blurb = "blurb"
-var project_funder = "UN"
 
-L.Control.textbox = L.Control.extend({
-	onAdd: function(map) {
-		var text = L.DomUtil.create('div', 'textbox-interior');
-		text.style.backgroundColor='white';
-		text.style.padding= '10px';
-		text.style.height = '500px';
-		text.style.width = '100px';
-		text.id = "info_text";
-		//add text to textbox
-		text.innerHTML = "<strong>More Info</strong><div>" + project_title + "</div><div>" + project_blurb + "</div>";
-		return text; 
-	},
+function onMarkerClick(e){
 
-	onRemove: function(map) {
+	try { $( "#info_text" ).remove() }
+	catch(err) { }
+	//create panel with all info here
 
+	//get project data from event data
+	var project_title = e.target.feature.properties.SHORT_N
+	var project_overall_pi = e.target.feature.properties.OVERALL
+	var project_author = e.target.feature.properties.TEG_INV
+	var project_blurb = e.target.feature.properties.PROJECT_N
+	var project_start_date = e.target.feature.properties.START_D
+	var project_end_date = e.target.feature.properties.END_DAT
+	var project_funder = e.target.feature.properties.PROJECT_F
+
+	var info_elements = [project_title, project_overall_pi, project_author, project_blurb, project_start_date, project_end_date, project_funder]
+
+	//format data into html (omit missing items)
+	var project_title_HTML = "<div><h2>" + project_title + "</h2></div>"
+	var project_overall_pi_HTML = "<div><h4>" + project_overall_pi + "</h4></div>"
+	var project_author_HTML = "<div><h4>" + project_author + "</h4></div>"
+	var project_blurb_HTML = "<div><p>" + project_blurb + "</p></div>"
+	
+	//handle this datetime format to make more natural
+	var project_start_date_HTML = "<div><p>" + project_start_date + "</p></div>"
+	var project_end_date_HTML = "<div><p>" + project_end_date + "</p></div>"
+	var project_funder_HTML = "<div><p>" + project_funder + "</p></div>"
+
+	var html_elements = [project_title_HTML, project_overall_pi_HTML, project_author_HTML, project_blurb_HTML, project_start_date_HTML, project_end_date_HTML, project_funder_HTML]
+
+	var null_elements = [];
+
+	for (var i in info_elements){
+		if (info_elements[i] == null){
+			null_elements.push(parseInt(i))
+		}
 	}
-});
 
-L.control.textbox = function(opts) {return new L.Control.textbox(opts);}
-var info_pane = L.control.textbox({ position: 'topright' }).addTo(main_map)
+	for (var i = null_elements.length -1; i >= 0; i--) {
+                html_elements.splice(null_elements[i], 1); 
+	}
 
-//remove panel after one second - remove when click is not on a point
-console.log(info_pane)
-setTimeout(function(){ info_pane.remove() }, 1000);
+	info_panel_text = html_elements.join("")
 
-//make geojson point file R, QGIS, host on github - call here - update textbox on marker click if click not on point, close textbox 
+	//create a new control with project data
+	L.Control.textbox = L.Control.extend({
+		onAdd: function(map) {
+			var text = L.DomUtil.create('div', 'textbox-interior');
+			text.style.backgroundColor='white';
+			text.style.padding= '10px';
+			text.style.height = '500px';
+			text.style.width = '100px';
+			text.id = "info_text";
+			//add text to textbox
+			text.innerHTML = info_panel_text;
+			return text; 
+		},
 
-//https://raw.githubusercontent.com/hamishgibbs/Interactive_Dashboard_Public_Data/master/Centre_Points.geojson
+		onRemove: function(map) {
 
-//Update text in the textbox: Using jquery, $("#info_text")[0].innerHTML = 'new text' 
+		}
+	});
 
-/*
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature, //you need to change this
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
+	L.control.textbox = function(opts) {return new L.Control.textbox(opts);}
+
+	var info_pane = L.control.textbox({ position: 'topright' }).addTo(main_map)
+
+	function MapClick(e) {
+		info_pane.remove()
+	}
+
+	main_map.on('click', MapClick);
+	
 }
-*/
+
+//change popup styling, map marker styling, panel styling & width
+
+//add everything to a single HTML document
